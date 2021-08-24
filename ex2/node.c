53ed30d3ee584098d387323f6ff9fb7566351b70
MUHAMMAD NIAAZ WAHAB <niaaz@xcne5.comp.nus.edu.sg>
2021-08-25 01:23:47 +0800

    add task 2, 3

--- ex2/node.c
+++ ex2/node.c
@@ -19,15 +19,104 @@
 // starting at 0).
 // Note: index is guaranteed to be valid.
 void insert_node_at(list *lst, int index, int data) {
-	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+	
 
+	// instantiate new node, add data 
+	node* newNode = (node*)malloc(sizeof(node));
 	newNode->data = data;
+	
+	// when list empty
+	if (lst->head == NULL) {
+		newNode->next = newNode;
+		lst->head = newNode;
+	}
+
+	// when index referenced is 0
+	else if (index == 0) {
+		newNode->next = lst->head;
+		// lst->head = newNode;
+
+		node* lastNode = (node*)malloc(sizeof(node));
+		lastNode = newNode->next;
+		while (lastNode->next != lst->head) {
+			lastNode = lastNode->next;
+		}
+		
+		lst->head = newNode;
+		lastNode->next = newNode;
+	}
+	
+	// when index in middle or  end
+	else {
+		node* previousNode = (node*)malloc(sizeof(node));
+		node* nextNode = (node*)malloc(sizeof(node));
+		
+		previousNode = lst->head;
+
+		for (int i = 0; i < index - 1; i++) {
+			previousNode = previousNode->next;
+		}
+		
+		nextNode = previousNode->next;
+
+		newNode->next = nextNode;
+		previousNode->next = newNode;
+	}	
 	
 }
 
 // Deletes node at index (counting from head starting from 0).
 // Note: index is guarenteed to be valid.
 void delete_node_at(list *lst, int index) {
+	
+	node* nodeToDelete = (node*)malloc(sizeof(node));
+	
+	if (lst->head == NULL) {
+		int i = 1;
+	}
+
+	else if (lst->head == lst->head->next) {
+		free(lst->head);
+		lst->head = NULL;
+	}
+
+	else if (index == 0) {
+		node* nextNode = (node*)malloc(sizeof(node));
+		node* lastNode = (node*)malloc(sizeof(node));
+
+		nodeToDelete = lst->head;
+		nextNode = nodeToDelete->next;
+		
+		lastNode = nodeToDelete->next;
+		while (lastNode->next != lst->head) {
+			lastNode = lastNode->next;
+		}
+		
+		lst->head = nextNode;
+		lastNode->next = nextNode;
+
+		free(nodeToDelete);
+
+	}
+
+	else {
+		node* previousNode = (node*)malloc(sizeof(node));
+		node* nextNode = (node*)malloc(sizeof(node));
+		
+		previousNode = lst->head;
+
+		for (int i = 0; i < index - 1; i++) {
+			previousNode = previousNode->next;
+		}
+
+		nodeToDelete = previousNode->next;
+		nextNode = nodeToDelete->next;
+
+		previousNode->next = nextNode;
+
+		free(nodeToDelete);
+
+	}
 }
 
 // Rotates list by the given offset.
@@ -44,3 +133,44 @@ void reverse_list(list *lst) {
 // any allocated memory in the process
 void reset_list(list *lst) {
 }
+
+
+int main() {
+	
+	printf("running...\n");
+	list* lst = (list*)malloc(sizeof(list));
+	
+	//node* tmp = (node*)malloc(sizeof(node));
+	//tmp->data = 5;
+	//lst->head = tmp;
+
+	insert_node_at(lst, 0, 1);
+	insert_node_at(lst, 1, 5);
+	insert_node_at(lst, 2, 8);
+	insert_node_at(lst, 3, 11);
+
+	delete_node_at(lst, 0);
+	delete_node_at(lst, 2);
+
+	node* curr = lst -> head;
+	while (curr->next != lst->head)  {
+		int data = curr -> data;
+		printf("%d\n", data);
+		curr = curr->next;
+	}
+
+	int data = curr-> data;
+	printf("%d\n", data);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
