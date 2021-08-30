d24e050613abc215bea3c552ec3b5d7f2e9b628f
MUHAMMAD NIAAZ WAHAB <niaaz@xcne5.comp.nus.edu.sg>
2021-08-30 18:01:10 +0800

    add ex3 almost finish

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,9 +1,9 @@
 /*************************************
-* Lab 1 Exercise 3
-* Name:
-* Student No:
-* Lab Group:
-*************************************/
+ * Lab 1 Exercise 3
+ * Name: Muhammad Niaaz Wahab
+ * Student No: A0200161E
+ * Lab Group: B04
+ *************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,19 +26,114 @@
 #define RESET_LIST 5
 #define MAP 6
 
+#define ADD_ONE 0
+#define ADD_TWO 1
+#define MULTIPLY_FIVE 2
+#define SQUARE 3
+#define CUBE 4
+
+void run_instructions(list *lst, int instr);
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Error: expecting 1 argument, %d found\n", argc - 1);
-        exit(1);
-    }
+		if (argc != 2) {
+				fprintf(stderr, "Error: expecting 1 argument, %d found\n", argc - 1);
+				exit(1);
+		}
+		
+		// We read in the file name provided as argument
+		char *fname = argv[1];
+		
+		print("hello");
+		// Update the array of function pointers
+		// DO NOT REMOVE THIS CALL
+		// (You may leave the function empty if you do not need it)
+		update_functions();
 
-    // We read in the file name provided as argument
-    char *fname = argv[1];
+		// Rest of code logic here
+		//if fname != ...		
 
-    // Update the array of function pointers
-    // DO NOT REMOVE THIS CALL
-    // (You may leave the function empty if you do not need it)
-    update_functions();
+		FILE *fptr = fopen(fname, "r");
+		
+		// file check
+		if (fptr == NULL) {
+				printf("Unable to open file\n");
+		}
 
-    // Rest of code logic here
+		list *lst = (list*)malloc(sizeof(lst));
+		lst->head = NULL;
+		int instr;
+
+		while (fscanf(fptr, "%d", &instr) == 1) {
+				printf("hello");
+				run_instructions(lst, instr);
+		}
+		
+		reset_list(lst);
+		free(lst);
 }
+
+void run_instructions(list *lst, int instr) {
+		int index, data, offset;
+		switch (instr) {
+				case SUM_LIST:
+						sum_list(lst);
+						break;
+				case INSERT_AT:
+						scanf("%d %d", &index, &data);
+						insert_node_at(lst, index, data);
+						break;
+				case DELETE_AT:
+						scanf("%d", &index);
+						delete_node_at(lst, index);
+						break;
+						rotate_list(lst, offset);
+						break;
+				case REVERSE_LIST:
+						reverse_list(lst);
+						break;
+				case RESET_LIST:
+						reset_list(lst);
+						break;
+				case MAP:
+						scanf("%d", &data);
+						map(lst, func_list[data]);
+						/*
+						switch (data) {
+								case ADD_ONE:
+										map(lst, func_list[0]);
+										break;
+								case ADD_TWO:
+										map(lst, func_list[1]);
+										break;
+								case MULTIPLY_FIVE:
+										map(lst, func_list[2]);
+										break;
+								case SQUARE:
+										map(lst, func_list[3]);
+										break;
+								case CUBE:
+										map(lst, func_list[4]);
+						*/
+						
+		}
+}
+
+/*
+void print_list(list *lst) {
+		if (lst->head == NULL) {
+				printf("[ ]\n");
+				return;
+		}
+
+		printf("[ ");
+		node *curr = lst->head;
+
+		do {
+				printf("%d ", curr->data);
+				curr = curr->next;
+		}
+
+		printf("]\n");
+}
+
+*/
